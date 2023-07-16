310271c9afd3993fb29a6acc65fd0ea7a42dc928
ElchaX <maciej.sorko@gmail.com>
2023-07-16 20:33:30 +0200

    1st component done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
+import css from './App.css';
+
+import Profile from './Profile/Profile.jsx';
+import user from './Profile/users.json';
+
+import data from './Statistics/data.json';
+import Statistics from './Statistics/Statistics.jsx';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework templatee
+    <div className={css.main}>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics stats={data} />
     </div>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types';
+import css from './statistic.module.css';
+
+const getRandomColor = () => {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+const Statistics = ({ title, stats }) => {
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+
+      <ul className={css.stat_list}>
+        {stats.map(stat => (
+          <li
+            key={stat.id}
+            className={css.stat_li_item}
+            style={{ backgroundColor: getRandomColor() }}
+          >
+            <span className={css.label}>{stat.label}</span>
+            <span className={css.percentage}>{stat.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.array.isRequired,
+};
+
+export default Statistics;
